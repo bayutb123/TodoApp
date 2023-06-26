90ac1673fb451db1a414b558924ecb57c20997d0
bayutb123 <bayutantra28@gmail.com>
2023-06-26 15:36:15 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
@@ -0,0 +1,38 @@
+package com.dicoding.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.RawQuery
+import androidx.room.Update
+import androidx.sqlite.db.SupportSQLiteQuery
+
+//TODO 2 : Define data access object (DAO) OK - CEK LAGI
+@Dao
+interface TaskDao {
+
+    @RawQuery(observedEntities = [Task::class])
+    fun getTasks(query: SupportSQLiteQuery): DataSource.Factory<Int, Task>
+
+    @Query("SELECT * FROM tasks WHERE id=:taskId")
+    fun getTaskById(taskId: Int): LiveData<Task>
+
+    @Query("SELECT * FROM tasks ORDER BY dueDateMillis ASC")
+    fun getNearestActiveTask(): Task
+
+    @Insert
+    suspend fun insertTask(task: Task): Long
+
+    @Insert
+    fun insertAll(vararg tasks: Task)
+
+    @Query("DELETE FROM tasks WHERE id=:task")
+    suspend fun deleteTask(task: Task)
+
+    @Query("UPDATE tasks SET isCompleted=:completed")
+    suspend fun updateCompleted(taskId: Int, completed: Boolean)
+
+}
---
+++ app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
@@ -0,0 +1,65 @@
+package com.dicoding.todoapp.data
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import androidx.room.Query
+import androidx.sqlite.db.SimpleSQLiteQuery
+import com.dicoding.todoapp.utils.FilterUtils
+import com.dicoding.todoapp.utils.TasksFilterType
+
+class TaskRepository(private val tasksDao: TaskDao) {
+
+    companion object {
+        const val PAGE_SIZE = 30
+        const val PLACEHOLDERS = true
+
+        @Volatile
+        private var instance: TaskRepository? = null
+
+        fun getInstance(context: Context): TaskRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    val database = TaskDatabase.getInstance(context)
+                    instance = TaskRepository(database.taskDao())
+                }
+                return instance as TaskRepository
+            }
+
+        }
+    }
+
+    //TODO 4 : Use FilterUtils.getFilteredQuery to create filterable query OK
+    //TODO 5 : Build PagedList with configuration OK
+    fun getTasks(filter: TasksFilterType): LiveData<PagedList<Task>> {
+        val config =
+            PagedList.Config.Builder().setMaxSize(PAGE_SIZE).setEnablePlaceholders(PLACEHOLDERS)
+                .build()
+        val filteredQuery = FilterUtils.getFilteredQuery(filter)
+        val taskData = tasksDao.getTasks(
+            query = filteredQuery
+        )
+        return LivePagedListBuilder(taskData, config).build()
+    }
+
+    fun getTaskById(taskId: Int): LiveData<Task> {
+        return tasksDao.getTaskById(taskId)
+    }
+
+    fun getNearestActiveTask(): Task {
+        return tasksDao.getNearestActiveTask()
+    }
+
+    suspend fun insertTask(newTask: Task): Long {
+        return tasksDao.insertTask(newTask)
+    }
+
+    suspend fun deleteTask(task: Task) {
+        tasksDao.deleteTask(task)
+    }
+
+    suspend fun completeTask(task: Task, isCompleted: Boolean) {
+        tasksDao.updateCompleted(task.id, isCompleted)
+    }
+}
